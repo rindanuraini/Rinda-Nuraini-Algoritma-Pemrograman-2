5e15dd9e084554f8cce4da88375aae7654b41688
rindanuraini <inspiritrinda@gmail.com>
2017-03-20 22:58:21 +0700

    Upload Kedua

---
+++ latihan2input.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+	char nama[13];
+	cout << "Nama : ";
+	cin >> nama;
+	return 0;
+}
---
+++ latihan2output.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+	cout << "Say Hello, Rinda Nuraini  \n";
+
+	cout << "Say Hello and please enter" << endl;
+	return 0;
+}
